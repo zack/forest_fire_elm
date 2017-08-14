e12e01a85f171bc484ddcb33b97b753cf6d76af2
Zack Youngren <zack@youngren.io>
2017-08-14 02:41:20 -0400

    Large number of performance improvements

--- ForestFire.elm
+++ ForestFire.elm
@@ -1,28 +1,32 @@
 module ForestFire exposing (..)
 
-import Array exposing (..)
-import Dict exposing (..)
+import AnimationFrame exposing (..)
+import Debug exposing (log)
 import Html exposing (..)
 import Html.Attributes exposing (class, id)
 import Html.Events exposing (onClick)
-import Random exposing (Generator, maxInt, minInt)
+import Random exposing (Generator, Seed, maxInt, minInt)
 import Set exposing (..)
 import Svg exposing (svg, rect)
 import Svg.Attributes exposing (version, viewBox, width, fill, x, y, height)
-import Time exposing (Time, millisecond)
+import Time exposing (..)
 
 
 type alias Model =
     { forest : Forest
     , running : Bool
-    , seed : Random.Seed
+    , seed : Seed
     , clock : Int
-    , speed : Float
     , size : Int
     , burnRate : Float
     , growthRate : Float
-    , probabilities : List Float
+    , animationSpeed : Float
+    , timeSinceDraw : Float
     }
+
+
+type alias Flags =
+    { randomSeed : Int }
 
 
 type alias Forest =
@@ -37,7 +41,6 @@ type Tree
 
 type Msg
     = InitForest Forest
-    | SetSeed Int
     | IncrementForest Time
     | ToggleRunning
     | Restart
@@ -45,64 +48,88 @@ type Msg
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    let
-        ( probabilities, seed ) =
-            getNewProbabilities model
-    in
-        case msg of
-            InitForest forest ->
-                ( { model
-                    | forest = forest
-                    , probabilities = probabilities
-                    , seed = seed
-                  }
-                , Random.generate SetSeed (Random.int maxInt minInt)
+    case msg of
+        InitForest forest ->
+            ( { model | forest = forest }
+            , Cmd.none
+            )
+
+        IncrementForest msg ->
+            if model.timeSinceDraw < model.animationSpeed then
+                ( { model | timeSinceDraw = (model.timeSinceDraw + (Time.inMilliseconds msg)) }, Cmd.none )
+            else
+                let
+                    ( newSeed, newForest ) =
+                        incrementForest model
+
+                    a =
+                        log "msg time" (toString msg)
+                in
+                    if model.running then
+                        ( { model
+                            | forest = newForest
+                            , seed = newSeed
+                            , clock = model.clock + 1
+                            , timeSinceDraw = 0
+                          }
+                        , Cmd.none
+                        )
+                    else
+                        ( model, Cmd.none )
+
+        ToggleRunning ->
+            ( { model | running = (not model.running) }, Cmd.none )
+
+        Restart ->
+            let
+                m =
+                    initModel model.seed
+            in
+                ( { m | running = model.running }
+                , Random.generate InitForest (forestGenerator model.size)
                 )
 
-            SetSeed seed ->
-                ( { model | seed = Random.initialSeed seed }, Cmd.none )
 
-            IncrementForest _ ->
-                if model.running then
-                    ( { model
-                        | forest = incrementForest model
-                        , probabilities = probabilities
-                        , seed = seed
-                        , clock = model.clock + 1
-                      }
-                    , Cmd.none
-                    )
-                else
-                    ( model, Cmd.none )
-
-            ToggleRunning ->
-                ( { model | running = (not model.running) }, Cmd.none )
-
-            Restart ->
-                ( { initModel | running = model.running }
-                , Random.generate InitForest (forestGenerator initModel)
-                )
-
-
-incrementForest : Model -> Forest
+incrementForest : Model -> ( Random.Seed, Forest )
 incrementForest model =
     let
-        arrProbabilities : Array Float
-        arrProbabilities =
-            Array.fromList model.probabilities
-
         getNeighbors : Int -> List Int
         getNeighbors idx =
-            [ idx - 1
-            , idx + 1
-            , idx - model.size
-            , idx - model.size - 1
-            , idx - model.size + 1
-            , idx + model.size
-            , idx + model.size - 1
-            , idx + model.size + 1
-            ]
-                |> List.filter (\idx -> idx >= 0 && idx < (model.size * model.size))
+            let
+                leftBorder =
+                    idx % model.size == 0
+
+                rightBorder =
+                    (idx + 1) % model.size == 0
+            in
+                [ if leftBorder then
+                    -1
+                  else
+                    idx - 1
+                , if rightBorder then
+                    -1
+                  else
+                    idx + 1
+                , idx - model.size
+                , if leftBorder then
+                    -1
+                  else
+                    idx - model.size - 1
+                , if rightBorder then
+                    -1
+                  else
+                    idx - model.size + 1
+                , idx + model.size
+                , if leftBorder then
+                    -1
+                  else
+                    idx + model.size - 1
+                , if rightBorder then
+                    -1
+                  else
+                    idx + model.size + 1
+                ]
+                    |> List.filter (\idx -> idx >= 0 && idx < (model.size * model.size))
 
         ignitingTrees : Forest -> Set Int
         ignitingTrees forest =
@@ -117,50 +144,47 @@ incrementForest model =
                 |> List.concat
                 |> Set.fromList
 
-        transformTree : Set Int -> Int -> Tree -> Tree
-        transformTree igniting idx tree =
+        reductionFunction : Tree -> ( Seed, Int, List Tree ) -> ( Seed, Int, List Tree )
+        reductionFunction tree ( seed, idx, forestSoFar ) =
             case tree of
-                Living ->
-                    if Set.member idx igniting then
-                        Burning
-                    else
-                        let
-                            prob =
-                                Maybe.withDefault 0 (Array.get idx arrProbabilities)
-                        in
-                            if prob < model.burnRate then
-                                Burning
-                            else
-                                Living
-
                 Burning ->
-                    Dead
+                    ( seed, idx + 1, Dead :: forestSoFar )
 
                 Dead ->
                     let
-                        prob =
-                            Maybe.withDefault 0 (Array.get idx arrProbabilities)
+                        ( randFloat, newSeed ) =
+                            Random.step (Random.float 0 1) seed
+
+                        newTree =
+                            if randFloat < model.growthRate then
+                                Living
+                            else
+                                Dead
                     in
-                        if prob < model.growthRate then
-                            Living
+                        ( newSeed, idx + 1, newTree :: forestSoFar )
+
+                Living ->
+                    let
+                        ( randFloat, newSeed ) =
+                            Random.step (Random.float 0 1) seed
+                    in
+                        if randFloat < model.burnRate || (Set.member idx burningNeighbors) then
+                            ( newSeed, idx + 1, Burning :: forestSoFar )
                         else
-                            Dead
+                            ( newSeed, idx + 1, Living :: forestSoFar )
+
+        burningNeighbors =
+            ignitingTrees model.forest
+
+        ( newSeed, _, newForest ) =
+            List.foldl reductionFunction ( model.seed, 0, [] ) model.forest
     in
-        let
-            igniting =
-                ignitingTrees model.forest
-        in
-            List.indexedMap (transformTree igniting) model.forest
-
-
-getNewProbabilities : Model -> ( List Float, Random.Seed )
-getNewProbabilities model =
-    Random.step (probabilityGenerator model) model.seed
+        ( newSeed, (List.reverse newForest) )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every (model.speed * millisecond) IncrementForest
+    AnimationFrame.diffs IncrementForest
 
 
 view : Model -> Html Msg
@@ -182,9 +206,6 @@ view model =
         livingTree x y =
             tree x y "#0EA27D"
 
-        deadTree x y =
-            tree x y "#1A58A3"
-
         drawTree idx type_ =
             let
                 y =
@@ -201,7 +222,7 @@ view model =
                         burningTree x y
 
                     Dead ->
-                        deadTree x y
+                        Svg.text ""
     in
         div [ class "content" ]
             [ h1 [] [ text ("Forest Fire (" ++ (toString model.clock) ++ ")") ]
@@ -215,7 +236,17 @@ view model =
                     , height "800"
                     , viewBox ("0 0 " ++ (toString model.size) ++ " " ++ (toString model.size))
                     ]
-                    (List.indexedMap drawTree model.forest)
+                    ((rect
+                        [ fill "#1A58A3"
+                        , x "0"
+                        , y "0"
+                        , width (toString model.size)
+                        , height (toString model.size)
+                        ]
+                        []
+                     )
+                        :: (List.indexedMap drawTree model.forest)
+                    )
                 ]
             ]
 
@@ -237,8 +268,8 @@ restartButton =
     button [ onClick Restart ] [ text "Restart" ]
 
 
-forestGenerator : Model -> Generator Forest
-forestGenerator model =
+forestGenerator : Int -> Generator Forest
+forestGenerator size =
     let
         intToTree : Int -> Tree
         intToTree val =
@@ -251,40 +282,41 @@ forestGenerator model =
         treeGenerator =
             Random.map intToTree (Random.int 0 1)
     in
-        Random.list (model.size * model.size) treeGenerator
-
-
-probabilityGenerator : Model -> Generator (List Float)
-probabilityGenerator model =
-    Random.list (model.size * model.size) (Random.float 0 1)
+        Random.list (size * size) treeGenerator
 
 
 
 -- Good starting parameters are size: 100, burnRate: 0.00001, growthRate: 0.01
 
 
-initModel : Model
-initModel =
+initModel : Seed -> Model
+initModel seed =
     { forest = []
     , running = True
-    , seed = Random.initialSeed 0
-    , speed = 1
+    , seed = seed
     , clock = 0
-    , size = 80
-    , burnRate = 0.00001
+    , size = 100
+    , burnRate = 0.0001
     , growthRate = 0.01
-    , probabilities = []
+    , animationSpeed = 1.0
+    , timeSinceDraw = 0.0
     }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initModel, Random.generate InitForest (forestGenerator initModel) )
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    let
+        model =
+            initModel (Random.initialSeed flags.randomSeed)
+    in
+        ( model
+        , Random.generate InitForest (forestGenerator model.size)
+        )
 
 
-main : Program Never Model Msg
+main : Program Flags Model Msg
 main =
-    Html.program
+    Html.programWithFlags
         { init = init
         , view = view
         , update = update
